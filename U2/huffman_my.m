d32c84783fd79ae33e418a90b49268a201700c48
Bastian Globig <bastian@globig-berlin.de>
2022-05-31 09:00:05 +0200

    Tune U2

--- U2/huffman_my.m
+++ U2/huffman_my.m
@@ -5,7 +5,7 @@ close all
 % Load Dictionary 
 dict = readtable('huff_s2202.dict', 'Format', '%s%s', 'FileType', 'Text', 'delimiter', ';');
 % Load Image
-img = imread('Laboruebung 1/s2201.ppm');
+img = imread('Laboruebung 1/s2202.ppm');
 % Convert to Gray
 gray = rgb2gray(img);
 [h,w] = size(gray);
@@ -16,39 +16,30 @@ imshow(img);
 
 % Schritt 1
 x = reshape(gray, [m 1]);
-% Read List of Codes
-c_ = cell(height(dict), 1);
-for i = 1:height(dict)
-    c_(i) = dict{i, 2};
-end
 
 % Schritt 2
 huff=cell(height(dict), 1);
 for i=1:m
-    v = x(i);
-    huff(i) = cellstr(dict{v+1, 2});
+    % Transition from [0..255] to [1..256]
+    huff(i) = cellstr(dict{x(i)+1, 2});
 end
 
 l = 0;
 
 % Schritt 3
 j=1;
-y=zeros(1, height(dict));
+y=zeros(height(dict), 1);
 for i = 1:length(huff)
     l = l + strlength(huff(i));
-    for j = 1:height(dict)
-        if strcmp(dict{j, 2}, huff(i))
-            y(i) = str2double(dict{j, 1});
-            break
-        end
-    end
+    f = find(strcmp([dict{:,2}], huff(i)));
+    y(i) = str2double(dict{f+1, 1});
 end
 
 assert(length(x)==length(y));
-assert(sum(x)==sum(y));
+assert(uint16(sum(x))==uint16(sum(y)));
 
 komp = l / (m*8);
 
 imgy = reshape (y, [h w]);
 figure('Name', 'Decoded');
-imshow(imgy);
+imshow(uint8(imgy));
